f3738e374ffc0ec1f7273cb9862629ff3e4282f0
Gokulraj0606 <gokul222.gr@gmail.com>
2022-11-18 14:14:57 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const users = [
+    {
+      name: "sethu",
+      pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Um8cYinLpQ9vPBUerzsWf2kApxrF38iWgw&usqp=CAU"
+    },
+    {
+      name: "resvanth",
+      pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq6eQppcP6_F1F7ji-mfmlZK4kDKztrktR4g&usqp=CAU"
+    },
+    {
+      name: "varunya",
+      pic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScYr4CcfLo-IfFqPgnocAr1bVdEQULTuIRFw&usqp=CAU"
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((tt) => (
+        <User name={tt.name} pic={tt.pic} />
+      ))}
+
     </div>
   );
+}
+function User({ name, pic }) {
+  return (
+    <div>
+      <img className='profile-pic' src={pic} alt={name} />
+
+      <h1>hello, <span className='usr-name'>{name}</span> </h1>
+    </div>
+  )
 }
 
 export default App;
